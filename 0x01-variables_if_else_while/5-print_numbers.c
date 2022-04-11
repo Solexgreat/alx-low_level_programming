6567707fa003e9acbe3c3c4301328c1e2927c153
Solexgreat <Solomongodkid>
2022-04-10 18:03:30 -0700

    Task 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%i\n", n);
+	}
+	n++;
+	return (0);
+}
