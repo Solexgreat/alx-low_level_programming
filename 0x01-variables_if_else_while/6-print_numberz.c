d29445f29a331edc22ab43db6757e5e673d6c37d
Solexgreat <Solomongodkid>
2022-04-10 18:34:30 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		int putchar(int n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
