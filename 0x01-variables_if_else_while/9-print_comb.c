9a3d55d3a7d7890bb5136e8f47a24b742710439f
Solexgreat <Solomongodkid>
2022-04-11 06:44:54 -0700

    Task 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+		if (n != 58)
+		{
+			putchar(',');
+			putchar(' ');
+			
+		}
+	}
+	putchar('\n');
+	return (0);
+}
