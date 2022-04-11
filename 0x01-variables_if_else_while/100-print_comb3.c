15c0c4fadd2c1c63164e0b3fabaa0d610f935ced
Solexgreat <Solomongodkid>
2022-04-11 15:46:10 -0700

    Task 10 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 48;
+	m = 48;
+
+	while (m < 58)
+	{
+		n = 48;
+
+		while (n < 58)
+		{
+			if (m != n && m < n)
+			{
+				putchar(m);
+				putchar(n);
+				if ( n == 57 && m == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+		}
+			n++;
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
