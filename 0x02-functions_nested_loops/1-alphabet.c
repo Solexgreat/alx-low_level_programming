02f229ddcb0a1a5d0ccf9a152b68b9cd1719ffb8
Solexgreat <Solomongodkid>
2022-04-12 16:18:09 -0700

    Task 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - Write a program that prints _putchar, followed by a new line.
+ * print_alphabet- Entry point
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char n;
-	
+
 	n = 'a';
 	while (n <= 'z')
 	{
