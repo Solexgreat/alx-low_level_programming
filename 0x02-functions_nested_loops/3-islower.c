44e25fb8919105d20b1af8a6938106ed8e2b6c52
Solexgreat <Solomongodkid>
2022-04-12 17:07:44 -0700

    Task 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _islower- Entry point
- *
+ * @c: character to check the case
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-	return ( c >= 97 && c <= 122);
+	return (c >= 97 && c <= 122);
 }
