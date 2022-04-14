b86c6d486bcafe3d764bd200f4a9653c81aa6f00
Solexgreat <Solomongodkid>
2022-04-14 06:36:50 -0700

    Task 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit-a function that checks for uppercase character
+ * @c : character check for a digit
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 58);
+}
