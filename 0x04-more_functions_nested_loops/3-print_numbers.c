25f80c5d0d45954043923f2f0a5e14c8577dd6c2
Solexgreat <Solomongodkid>
2022-04-14 07:41:11 -0700

    Task 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- a function that prints the numbers, from 0 t0 9
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	c = 48;
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+}
