15afd094fda70820b79b26255bfce98e885a1f66
Solexgreat <Solomongodkid>
2022-04-14 15:33:58 -0700

    Task 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *more_numbers- a function that prints the numbers, from 0 t0 14 ten
+ *times on a new line
+ * Return:0
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int n;
+
+	c = 0;
+	n = 0;
+	while (n <= 10)
+	{
+		c = 0;
+		while (c <= 14)
+		{
+			_putchar(c);
+			c++;
+		}
+		n++;
+	}
+	_putchar('\n');
+}
