79bb9310c3045eb2c9951d31d2beede1a8b9fe56
Solexgreat <Solomongodkid>
2022-04-15 18:53:26 -0700

    Task  9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square- a function that print square
+ *size times
+ *@size: Number of size
+ * Return:0
+ */
+
+void print_square(int size)
+{
+	int u;
+	int d;
+
+	if (size > 0)
+	{
+		for (u = 0; u < size; u++)
+		{
+			for (d = 0; d < size; d++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+
+		_putchar('\n');
+
+}
