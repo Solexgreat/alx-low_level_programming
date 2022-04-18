ebbe6c5e67ac21754721e9be04e1d52c4521c341
Solexgreat <Solomongodkid>
2022-04-18 05:04:20 -0700

    Task 11 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry 
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	unsigned long int n = 612852475143;
+	int p;
+	
+	for (p = 3; p <= 12057; p += 2)
+	{
+		for(c = 3; c <= p; c++)
+		{
+			while (n % p == 0 && n != p)
+				n /= p;
+		}
+		printf(" %lu \n",n);
+	}
+	return (0);
+} 
