c2487b6445069f901faaa329b41d37dbcdb3d384
Solexgreat <Solomongodkid>
2022-04-19 07:33:14 -0700

    Task  4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,14 +11,13 @@ void print_most_numbers(void)
 	int c;
 
 	c = 48;
-	while (c < 58)
+	for (; c < 58; c++)
 	{
 		if (c == 50 || c == 52)
 		{
 			continue;
 		}
 		_putchar(c);
-		c++;
 	}
 	_putchar('\n');
 }
