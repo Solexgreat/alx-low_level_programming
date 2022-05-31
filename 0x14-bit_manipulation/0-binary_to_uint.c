7e3971446ef86e1d5db7fd0df5bd1417ce144168
Solexgreat <Solomongodkid>
2022-05-31 16:00:15 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -25,7 +25,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[n] >= 65 && b[n] <= 122)
 			return (0);
-		else
+		if (b[n] >= '1' && b[n] <= '9')
 		{
 			q = (b[n] - '0') * i;
 			x += q;
