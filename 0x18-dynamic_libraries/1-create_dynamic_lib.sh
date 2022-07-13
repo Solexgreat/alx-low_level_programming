e2a3c97608b9c9278797e3e6e2ef543843b3d2f5
Solexgreat <Solomongodkid>
2022-07-12 18:49:55 -0700

    1 and 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc *.c -fPIC -c
-gc *.o -shared -o liball.so
+gcc *.o -shared -o liball.so
---
+++ 0x18-dynamic_libraries/c-func.c
@@ -0,0 +1,62 @@
+#include "list.h"
+#include <stdio.h>
+/**
+ * add - funtion that adds
+ * a - first integer
+ * b - second integer
+ * Return : Answer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - funtion that adds
+ * a - first integer
+ * b - second integer
+ * Return : Answer
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - funtion that adds
+ * a - first integer
+ * b - second integer
+ * Return : Answer
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - funtion that adds
+ * a - first integer
+ * b - second integer
+ * Return : Answer
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - funtion that adds
+ * a - first integer
+ * b - second integer
+ * Return : Answer
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
